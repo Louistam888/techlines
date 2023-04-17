7e3ebefb48dbed89521327106763303e686c6f95
Louistam888 <louistam888@gmail.com>
2023-04-17 19:59:14 -0400

    completed error handling for products.js

---
+++ client/src/screens/ProductScreen.jsx
@@ -0,0 +1,12 @@
+import { useParams } from  "react-router-dom";
+import { Box, Image, Text, Wrap, Stack, Spinner, Alert, AlertIcon, AlertDescription, AlertTitle, Flex, Badge, Heading, HStack, Button, SimpleGrid, useToast } from "@chakra-ui/react";
+
+
+
+const ProductScreen = () => {
+  return (
+    <div>ProductScreen</div>
+  )
+}
+
+export default ProductScreen
