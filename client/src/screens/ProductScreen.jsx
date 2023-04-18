b80507264eefd1ad001fb54a9a4d3aaa66423d52
Louistam888 <louistam888@gmail.com>
2023-04-17 20:58:17 -0400

    product screen part 3

--- client/src/screens/ProductScreen.jsx
+++ client/src/screens/ProductScreen.jsx
@@ -44,7 +44,70 @@ const ProductScreen = () => {
 
   return (
     <Wrap spacing="30px" justify="center" minHeight="100vh">
-      { loading ? <p>loading</p> : error ? <p>error</p> : product && <p>product </p>}
+      { loading 
+      
+      ? 
+      <Stack direction ="row" spacing={4}>
+        <Spinner mt={20} thickness ="2px" speed="0.65s" emptyColor="gray.200" color="orange.500" size="xl" />  
+      </Stack>
+      : error 
+        ? <Alert status="error">
+            <AlertIcon />
+            <AlertTitle>
+              We are sorry!
+            </AlertTitle>
+            <AlertDescription>
+              {error}
+            </AlertDescription>
+          </Alert>
+        : product && 
+          <Box maxW={{base: "3xl", lg: "5xl"}} mx="auto" px={{base : "4", md: "8", lg: "12"}} py={{base : "6", md: "8", lg: "12"}}>
+            <Stack direction={{base: "column", lg: "row"}} align={{lg:"flex-start"}}>
+              <Stack pr={{base: "0", md:"12"}} spacing={{base:"8", md: "4"}} flex="1.5" mb={{base: 12, md: "none"}}>
+                {product.productIsNew && ( 
+                  <Badge rounded="full" w="40px" fontSize="0.8em" colorScheme="green">
+                    New
+                  </Badge>
+                )}
+                <Heading fontSize="2xl" fontWeight="extrabold">
+                  {product.name}
+                </Heading>
+                <Stack spacing="5">
+                  <Box>
+                    <Text fontSize="xl">
+                      ${product.price}
+                    </Text>
+                    <Flex>
+                      <HStack spacing="2px">
+                        <StarIcon color="orange.500" />
+                        <StarIcon color={product.rating >=2 ? "orange.500" : "gray.200"} />
+                        <StarIcon color={product.rating >=3 ? "orange.500" : "gray.200"} />
+                        <StarIcon color={product.rating >=4 ? "orange.500" : "gray.200"} />
+                        <StarIcon color={product.rating >=5 ? "orange.500" : "gray.200"} />
+                      </HStack>
+                      <Text fontSize="md" fontWeight="bold" ml="4px">
+                        {product.numberOfReviews} Reviews
+                      </Text>
+                    </Flex>
+                  </Box>
+                  <Text>
+                    {product.description}
+                  </Text>
+                  <Text fontWeight={"bold"}>
+                    Quantity
+                  </Text>
+                  <Flex w="170px" p="5px" border="1px" borderColor="gray.200" alignItems="center">
+                    <Button disabled>
+                      <MinusIcon>
+
+                      </MinusIcon>
+                    </Button>
+                  </Flex>
+                </Stack>
+              </Stack>
+            </Stack>
+          </Box>
+      }
     </Wrap>
   ); 
 };
