cfac396725341acb4af1f41629a77f702a3900d5
Louistam888 <louistam888@gmail.com>
2023-04-23 00:05:36 -0400

    completed footer

--- client/src/screens/LandingScreen.jsx
+++ client/src/screens/LandingScreen.jsx
@@ -9,54 +9,52 @@ import {
   Skeleton,
   Stack,
   useColorModeValue,
+  Text
 } from '@chakra-ui/react'
-import { FaArrowRight } from 'react-icons/fa'
+import { FaArrowRight } from 'react-icons/fa';
+import { Link as ReactLink } from "react-router-dom";
+import { GiTechnoHeart } from "react-icons/gi";
 
 export const LandingScreen = () => (
-  <Box maxW="7xl" mx="auto" px={{ base: '0', lg: '12' }} py={{ base: '0', lg: '12' }}>
+  <Box maxW="8xl" mx="auto" px={{ base: '0', lg: '12' }} py={{ base: '0', lg: '12' }} minH="6x1">
     <Stack direction={{ base: 'column-reverse', lg: 'row' }} spacing={{ base: '0', lg: '20' }}>
       <Box
         width={{ lg: 'sm' }}
         transform={{ base: 'translateY(-50%)', lg: 'none' }}
-        bg={{ base: useColorModeValue('red.50', 'gray.700'), lg: 'transparent' }}
+        bg={{ base: useColorModeValue('orange.50', 'gray.700'), lg: 'transparent' }}
         mx={{ base: '6', md: '8', lg: '0' }}
         px={{ base: '6', md: '8', lg: '0' }}
         py={{ base: '6', md: '8', lg: '12' }}
       >
         <Stack spacing={{ base: '8', lg: '10' }}>
           <Stack spacing={{ base: '2', lg: '4' }}>
-            <Heading size="xl" color={useColorModeValue('red.500', 'red.300')}>
-              Misguided
-            </Heading>
+            <Flex alignItems="center">
+              <Icon as={GiTechnoHeart} h={12} w={12} color={useColorModeValue('orange.500', 'orange.300')} />
+            </Flex>  
+            <Text fontSize="4xl" fontWeight="bold">
+              Tech Lines
+            </Text>
             <Heading size="xl" fontWeight="normal">
-              Refresh your wardrobe
+              Refresh your equipment
             </Heading>
           </Stack>
           <HStack spacing="3">
-            <Link color={useColorModeValue('red.500', 'red.300')} fontWeight="bold" fontSize="lg">
+            <Link as={ReactLink} to="/" color={useColorModeValue('orange.500', 'orange.300')} fontWeight="bold" fontSize="lg">
               Discover now
             </Link>
-            <Icon color={useColorModeValue('red.500', 'red.300')} as={FaArrowRight} />
+            <Icon color={useColorModeValue('orange.500', 'orange.300')} as={FaArrowRight} />
           </HStack>
         </Stack>
       </Box>
       <Flex flex="1" overflow="hidden">
         <Image
-          src="https://images.unsplash.com/photo-1589156229687-496a31ad1d1f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+          src="images/landing.jpg"
           alt="Lovely Image"
           fallback={<Skeleton />}
-          maxH="450px"
+          maxH="550px"
           minW="300px"
           objectFit="cover"
           flex="1"
-        />
-        <Image
-          display={{ base: 'none', sm: 'initial' }}
-          src="https://images.unsplash.com/photo-1589156206699-bc21e38c8a7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-          alt="Lovely Image"
-          fallback={<Skeleton />}
-          maxH="450px"
-          objectFit="cover"
         />
       </Flex>
     </Stack>
