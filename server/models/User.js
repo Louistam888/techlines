73433bc8360a110cc39b8877af4031c9c978781b
Louistam888 <louistam888@gmail.com>
2023-04-23 16:04:49 -0400

    added userRoutes

--- server/models/User.js
+++ server/models/User.js
@@ -20,3 +20,20 @@ const userSchema = new.mongoose.Schema({
     default: false,
   },  
 }, {timestamps: true});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function(next) { 
+  if (!this.isModified("password")) {
+    next();
+  };
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const user = mongoose.mode("User", userSchema);
+
+export default User; 
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import User from "../models/user";
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+//TODO: redefine expiresIn 
+const genToken = (id) => {
+  return jwt.sign({id}, process.env.TOKEN_SECRET, {expiresIn: "60d"})
+};
+
+const loginUser = asyncHandler(async(req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({email})
+  if (user && (await user.matchPasswords(password))) {
+    res.json({
+      _id: user._id, 
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: genToken(user._id)
+    });
+  } else { 
+    res.status(401);
+    throw new error("Invalid email or password.");
+  }
+});
