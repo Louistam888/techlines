11768ce0d914be54c5b9cb2ba40d00208069bef8
Louistam888 <louistam888@gmail.com>
2023-04-29 11:57:27 -0400

    completed login and logout functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,27 @@
-import { Box, Flex, HStack, Link, IconButton, Icon, Text, useDisclosure, Button, Stack, useColorModeValue, useColorMode, useToast } from "@chakra-ui/react";
+import { 
+  Box, 
+  Flex, 
+  HStack, 
+  Link, 
+  IconButton, 
+  Icon, 
+  Text, 
+  useDisclosure, 
+  Button, 
+  Stack, 
+  useColorModeValue, 
+  useColorMode, 
+  useToast, 
+  MenuButton,
+  MenuDivider,
+  Menu,
+  MenuList,
+  MenuItem
+} from "@chakra-ui/react";
 import { Link as ReactLink} from "react-router-dom";
-import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon} from "@chakra-ui/icons";
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { CgProfile } from "react-icons/cg";
+import { MdLocalShipping, MdLogout } from "react-icons/md";
 import { GiTechnoHeart } from "react-icons/gi";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -68,7 +89,34 @@ const Navbar = () => {
                 onClick={()=> toggleColorMode()}
               />
             </NavLink>
-            {userInfo ? (<p>logged in</p>): (
+            {userInfo ? (
+              <>
+                <Menu>
+                  <MenuButton px="4" py="2" transition="all 0.3s" as={Button}>
+                    {userInfo.name} <ChevronDownIcon />
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem as={ReactLink} to="/profile">
+                      <CgProfile/>
+                      <Text ml="2">
+                        Profile
+                      </Text>
+                    </MenuItem>
+                    <MenuItem as={ReactLink} to="/your-orders">
+                      <MdLocalShipping/>
+                      <Text ml="2">
+                        Your Orders
+                      </Text>
+                    </MenuItem>
+                    <MenuDivider />
+                    <MenuItem onClick={logoutHandler}>
+                      <MdLogout />
+                      <Text ml="2">Logout</Text>
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </>
+            ) : ( 
               <>
                 <Button as={ReactLink} to="/login" p={2} fontSize="sm" fontWeight={400} variant="link">
                   Sign In
