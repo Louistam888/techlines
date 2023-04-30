3c1fc0af59b8c754be5746363de238fa5a6c2c83
Louistam888 <louistam888@gmail.com>
2023-04-30 15:53:56 -0400

    profilescreen part one

---
+++ client/src/screens/ProfileScreen.jsx
@@ -0,0 +1,44 @@
+import {
+  Box, 
+  Button,
+  FormControl,
+  Heading,
+  HStack,
+  Stack,
+  Text,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Flex,
+  Card,
+  CardHeader,
+  CartBody,
+  StackDivider, 
+  useToast
+ } from "@chakra-ui/react";
+import TextField from "../components/TextField";
+import PasswordTextField from "../components/PasswordTextField";
+import { useEffect, useState } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useDispatch, useSelector } from "react-redux";
+import { updateProfile, resetUpdateSuccess } from "../redux/actions/userActions";
+import { userLocation } from "react-router";
+import { Navigate } from "react-router-dom";
+
+
+
+const ProfileScreen = () => {
+
+  const dispatch = useDispatch();
+  const user = useSelecto((state) => state.user);
+  const { userInfo, error, loading, updateSuccess } = user;
+  const location = useLocation();
+  const toast = useToast();
+
+
+  return userInfo ? <p>profile screen</p> : <Navigate to="/login" replace={true} state={{from: location}}/>;
+
+};
+
+export default ProfileScreen;
