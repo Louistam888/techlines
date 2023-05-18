f54d64f3a88cb30e38996e5a54ea3a0ec586b07c
Louistam888 <louistam888@gmail.com>
2023-05-17 21:02:59 -0400

    completed orders screen pt2

--- client/src/screens/YourOrdersScreen.jsx
+++ client/src/screens/YourOrdersScreen.jsx
@@ -33,24 +33,25 @@ const YourOrdersScreen = () => {
       dispatch(getUserOrders());
     }
   }, []);
+
   return userInfo ? (
     <>
       {loading ? (
-        <Wrap justify="center" direction="column" align="center" mt="20px" minH="100vh">
-          <Stack direction="row" spacing={4}>
-            <Spinner mt={20} thickness={4} speed="0.65s" emptyColor="gray.200" color="orange.500" size="xl" />
+        <Wrap justify='center' direction='column' align='center' mt='20px' minH='100vh'>
+          <Stack direction='row' spacing={4}>
+            <Spinner mt={20} thickness='2px' speed='0.65s' emptyColor='gray.200' color='orange.500' size='xl' />
           </Stack>
         </Wrap>
       ) : error ? (
-        <Alert status="error">
+        <Alert status='error'>
           <AlertIcon />
           <AlertTitle>We are sorry!</AlertTitle>
           <AlertDescription>{error}</AlertDescription>
         </Alert>
       ) : (
         orders && (
-          <TableContainer minHeight="100vh">
-            <Table variant="striped">
+          <TableContainer minHeight='100vh'>
+            <Table variant='striped'>
               <Thead>
                 <Tr>
                   <Th>Order Id</Th>
@@ -61,10 +62,10 @@ const YourOrdersScreen = () => {
                 </Tr>
               </Thead>
               <Tbody>
-                {orders.map((order) => {
+                {orders.map((order) => (
                   <Tr key={order._id}>
                     <Td>{order._id}</Td>
-                    <Td>{new Date(order.createAt).toDateString()}</Td>
+                    <Td>{new Date(order.createdAt).toDateString()}</Td>
                     <Td>
                       ${order.totalPrice} via {order.paymentMethod}
                     </Td>
@@ -72,16 +73,16 @@ const YourOrdersScreen = () => {
                       {order.orderItems.map((item) => (
                         <UnorderedList key={item._id}>
                           <ListItem>
-                            {item.qty} x {item.name} (${item.price} each)
+                            {item.qty} x {item.name} (${item.price} ech)
                           </ListItem>
                         </UnorderedList>
                       ))}
                     </Td>
-                    <Td variant="outline">
-                      <Button>Receipt</Button>
+                    <Td>
+                      <Button variant='outline'>Receipt</Button>
                     </Td>
-                  </Tr>;
-                })}
+                  </Tr>
+                ))}
               </Tbody>
             </Table>
           </TableContainer>
@@ -89,7 +90,7 @@ const YourOrdersScreen = () => {
       )}
     </>
   ) : (
-    <Navigate to="/login" replace={true} state={{ from: location }} />
+    <Navigate to='/login' replace={true} state={{ from: location }} />
   );
 };
 
