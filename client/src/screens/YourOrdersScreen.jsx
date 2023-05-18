8f86fe95063cba451d235333471d8034b2e696cb
Louistam888 <louistam888@gmail.com>
2023-05-17 20:54:27 -0400

    orders screen part 2 started

--- client/src/screens/YourOrdersScreen.jsx
+++ client/src/screens/YourOrdersScreen.jsx
@@ -1,40 +1,96 @@
-import { 
-  TableContainer, 
-  Stack, 
-  Spinner, 
-  Alert, 
-  AlertIcon, 
-  AlertDescription, 
-  Th, 
-  Tbody, 
-  Tr, 
-  Thead, 
+import {
+  TableContainer,
+  Stack,
+  Spinner,
+  Alert,
+  AlertIcon,
+  AlertDescription,
+  Th,
+  Tbody,
+  Table,
+  Tr,
+  Td,
+  Thead,
   Button,
   ListItem,
-  UnorderedList, 
-  Wrap
+  UnorderedList,
+  Wrap,
+  AlertTitle,
 } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserOrders } from "../redux/actions/userActions";
 import { useEffect } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
-
 const YourOrdersScreen = () => {
-
   const dispatch = useDispatch();
-  const user = useSelector((state)=> state.user)
+  const user = useSelector((state) => state.user);
   const { loading, error, orders, userInfo } = user;
   const location = useLocation();
 
-  useEffect(()=> {
-    if(userInfo) {
-      dispatch(getUserOrders())
+  useEffect(() => {
+    if (userInfo) {
+      dispatch(getUserOrders());
     }
-  })
-  return (
-    userInfo ? (<p>Orders</p>) : <Navigate to="/login" replace={true} state={{from: location}} />
-  )
-}
+  }, []);
+  return userInfo ? (
+    <>
+      {loading ? (
+        <Wrap justify="center" direction="column" align="center" mt="20px" minH="100vh">
+          <Stack direction="row" spacing={4}>
+            <Spinner mt={20} thickness={4} speed="0.65s" emptyColor="gray.200" color="orange.500" size="xl" />
+          </Stack>
+        </Wrap>
+      ) : error ? (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle>We are sorry!</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      ) : (
+        orders && (
+          <TableContainer minHeight="100vh">
+            <Table variant="striped">
+              <Thead>
+                <Tr>
+                  <Th>Order Id</Th>
+                  <Th>Order Date</Th>
+                  <Th>Paid Total</Th>
+                  <Th>Items</Th>
+                  <Th>Print Receipt</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {orders.map((order) => {
+                  <Tr key={order._id}>
+                    <Td>{order._id}</Td>
+                    <Td>{new Date(order.createAt).toDateString()}</Td>
+                    <Td>
+                      ${order.totalPrice} via {order.paymentMethod}
+                    </Td>
+                    <Td>
+                      {order.orderItems.map((item) => (
+                        <UnorderedList key={item._id}>
+                          <ListItem>
+                            {item.qty} x {item.name} (${item.price} each)
+                          </ListItem>
+                        </UnorderedList>
+                      ))}
+                    </Td>
+                    <Td variant="outline">
+                      <Button>Receipt</Button>
+                    </Td>
+                  </Tr>;
+                })}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        )
+      )}
+    </>
+  ) : (
+    <Navigate to="/login" replace={true} state={{ from: location }} />
+  );
+};
 
-export default YourOrdersScreen
+export default YourOrdersScreen;
