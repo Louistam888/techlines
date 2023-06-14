7be0ef50c4685ec1515f180282b7f55886886517
Louistam888 <louistam888@gmail.com>
2023-06-14 18:54:27 -0400

    corrected middleware typo

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import Product from '../models/Product.js';
 import asyncHandler from 'express-async-handler';
 import User from '../models/User.js';
-import { protectRoute, admin } from '../middleware/authMiddleware.js';
+import { protectRoute, admin } from '../middleware/authMiddleWare.js';
 
 const productRoutes = express.Router();
 
