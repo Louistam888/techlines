332d640f5ae19249fb8fa4c1e8c5b9e9f2d89c84
Louistam888 <louistam888@gmail.com>
2023-06-14 12:26:56 -0400

    paypal fix

--- client/src/components/PayPalButton.jsx
+++ client/src/components/PayPalButton.jsx
@@ -1,27 +1,28 @@
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-import { Spinner, Stack } from "@chakra-ui/react";
-import { useState } from "react";
-import PAYPAL_CLIENT_ID from "../client_id";
-import axios from "axios";
-import { useEffect } from "react";
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+import { Spinner, Stack } from '@chakra-ui/react';
+import { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const PayPalButton = ({ total, onPaymentSuccess, onPaymentError, disabled }) => {
   const [paypalClient, setPayPalClient] = useState(null);
 
   useEffect(() => {
-    const paypalKey = async () => {
-      const { data: clientId } = await axios.get("/api/config/paypal");
+    const paypalkey = async () => {
+      const { data: clientId } = await axios.get('/api/config/paypal');
       setPayPalClient(clientId);
     };
-    paypalKey();
+    paypalkey();
   }, [paypalClient]);
-
   return !paypalClient ? (
-    <Stack direction="row" spacing={4} alignSelf="center">
-      <Spinner mt={20} thickness={4} speed="0.65s" emptyColor="gray.200" color="orange.500" size="xl" />
+    <Stack direction='row' spacing={4} alignSelf='center'>
+      <Spinner mt={20} thickness='2px' speed='0.65s' emptyColor='gray.200' color='orange.500' size='xl' />
     </Stack>
   ) : (
-    <PayPalScriptProvider options={{ "client-id": paypalClient }}>
+    <PayPalScriptProvider
+      options={{
+        'client-id': paypalClient,
+      }}>
       <PayPalButtons
         disabled={disabled}
         forceReRender={[total(), paypalClient]}
